286b6f4cc4c08d005ccf5cbc554003d3b2de18fc
dergaleva <diana.dergaleva@gmail.com>
2025-05-25 02:14:18 +0300

    добавила обработку данных

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, WebAppInfo
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
 
-# Вставь сюда свой токен от BotFather
 BOT_TOKEN = '8074180837:AAFmccQgXhctIaGS0wz7rswBepqADYX74rM'
-
-WEB_APP_URL = 'https://ваша-ссылка-Love-на-мини-приложение.vercel.app'
+WEB_APP_URL = 'https://love-calendar-553a.vercel.app/'
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     keyboard = [
-        [InlineKeyboardButton("Открыть календарь", web_app={"url": WEB_APP_URL})]
+        [InlineKeyboardButton("Открыть календарь", web_app=WebAppInfo(url=WEB_APP_URL))]
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
-    await update.message.reply_text("Привет! Нажми, чтобы открыть календарь:", reply_markup=reply_markup)
+    await update.message.reply_text("Привет! Открой календарь:", reply_markup=reply_markup)
+
+# Обработка данных от мини-приложения
+async def handle_web_app_data(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    data = update.message.web_app_data.data  # Получаем данные из мини-приложения
+    await update.message.reply_text(f"Ты добавила событие: {data}")
 
 app = ApplicationBuilder().token(BOT_TOKEN).build()
 app.add_handler(CommandHandler("start", start))
+app.add_handler(MessageHandler(filters.StatusUpdate.WEB_APP_DATA, handle_web_app_data))
 
 app.run_polling()
